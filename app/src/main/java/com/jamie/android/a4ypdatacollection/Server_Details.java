7d5ddffffbc51ccdce002c6dda0837ef99f7cd14
Jamie Brynes <jamiebrynes7@gmail.com>
2017-01-12 17:03:37 +0000

    chg: Add server details

---
+++ app/src/main/java/com/jamie/android/a4ypdatacollection/Server_Details.java
@@ -0,0 +1,9 @@
+package com.jamie.android.a4ypdatacollection;
+
+/**
+ * Created by Jamie Brynes on 10/6/2016.
+ */
+public class Server_Details {
+
+    public final static String SERVER_URL = "https://jamiebrynes.com/php/4yp_data_collection.php";
+}
